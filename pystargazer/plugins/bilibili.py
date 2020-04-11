0ff899abb4bb20161f8caa264a34d237c20b0eb1
PhotonQuantum <cy.n01@outlook.com>
2020-04-12 01:19:33 +0800

    feat(bilibili): add bilibili source

---
+++ pystargazer/plugins/bilibili.py
@@ -0,0 +1,77 @@
+import asyncio
+import json
+
+from httpx import AsyncClient
+
+from pystargazer.app import app
+from pystargazer.models import Event, KVPair
+
+
+class Bilibili:
+    def __init__(self):
+        self.client = AsyncClient()
+
+    async def fetch(self, user_id: int, since_id: int = 1):
+        url = "https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/space_history"
+        payload = {
+            "visitor_uid": 0,
+            "host_uid": user_id,
+            "offset_dynamic_id": 0,
+            "need_top": 0
+        }
+
+        r = (await self.client.get(url, params=payload)).json()
+
+        try:
+            # noinspection PyTypeChecker
+            dyn = json.loads(r["data"]["cards"][0]["card"])["item"]
+        except json.JSONDecodeError:
+            print("Bilibili Dynamic read error")
+            return since_id, None
+
+        dyn_id = dyn["id"]
+        if dyn_id == since_id:
+            return since_id, None
+
+        dyn_description = dyn["description"]
+        dyn_photos = [entry["img_src"] for entry in dyn["pictures"]]
+
+        return dyn_id, (dyn_description, dyn_photos)
+
+
+bilibili = Bilibili()
+
+
+@app.on_startup
+async def bilibili_setup():
+    if await app.plugin_state.get("bilibili_since") is None:
+        await app.plugin_state.put(KVPair("bilibili_since", {}))
+
+
+@app.scheduled("interval", seconds=10)
+async def bilibili_task():
+    b_since: KVPair = await app.plugin_state.get("bilibili_since")
+
+    b_valid_ids = []
+    b_names = []
+    # noinspection PyTypeChecker
+    async for vtuber in app.vtubers.has_field("bilibili"):
+        b_names.append(vtuber.key)
+        b_valid_ids.append(vtuber.value["bilibili"])
+
+    dyns = await asyncio.gather(*(bilibili.fetch(b_id, b_since.value.get(b_name, 1))
+                                  for b_name, b_id in zip(b_names, b_valid_ids)))
+
+    valid_dyns = {name: dyn for name, dyn in zip(b_names, dyns) if dyn[1]}
+    since = {name: dyn[0] for name, dyn in valid_dyns.items()}
+    b_since.value.update(since)
+    await app.plugin_state.put(b_since)
+
+    events = (
+        Event(
+            "bili_dyn",
+            name,
+            {"text": dyn[1][0], "images": dyn[1][1]}
+        )
+        for name, dyn in valid_dyns.items())
+    await asyncio.gather(*(app.send_event(event) for event in events))
