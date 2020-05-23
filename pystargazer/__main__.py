c015bd119db52e62782bb76b396a2f9a5c388b00
PhotonQuantum <self@lightquantum.me>
2020-05-23 18:39:51 +0800

    style: module is imported with 'import' and 'imported from'

--- pystargazer/__main__.py
+++ pystargazer/__main__.py
@@ -1,7 +1,6 @@
 import importlib
 import logging
-import os
-from os import path
+from os import environ, listdir, path
 
 from uvicorn.main import Config, Server
 
@@ -9,17 +8,17 @@ from .app import app
 from .models import KVContainer
 from .utils import strtobool
 
-debug = strtobool(os.environ.get("DEBUG"))
-access_log = strtobool(os.environ.get("ACCESS_LOG"), True)
-host = os.environ.get("HOST", "0.0.0.0")
-port = int(os.environ.get("PORT", "80"))
+debug = strtobool(environ.get("DEBUG"))
+access_log = strtobool(environ.get("ACCESS_LOG"), True)
+host = environ.get("HOST", "0.0.0.0")
+port = int(environ.get("PORT", "80"))
 
 app._vtubers = KVContainer(app.credentials.get("vtubers_storage"), "vtubers")
 app._configs = KVContainer(app.credentials.get("configs_storage"), "configs")
 app._states = KVContainer(app.credentials.get("plugins_storage"), "states")
 
 plugin_dir = path.join(path.dirname(path.abspath(__file__)), "plugins")
-plugins_path = [file[:-3] for file in os.listdir(plugin_dir) if file.endswith(".py")]
+plugins_path = [file[:-3] for file in listdir(plugin_dir) if file.endswith(".py")]
 plugins = {plugin.__name__: plugin for plugin in
            [importlib.import_module(f"pystargazer.plugins.{plugin_path}") for plugin_path in plugins_path]}
 
