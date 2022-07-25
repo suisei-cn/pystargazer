1e56d0a492dafb64e17eaf45bc6079e8a12467d6
LightQuantum <self@lightquantum.me>
2022-07-25 13:28:47 +0800

    fix: include subpackages

--- setup.py
+++ setup.py
@@ -1,39 +1,39 @@
 #!/usr/bin/env python
 
-from setuptools import find_packages, setup
+from setuptools import find_namespace_packages, setup
 
 setup(
-        name='pystargazer',
-        version='0.2.2',
-        description='A flexible vtuber tracker.',
-        author='LightQuantum',
-        author_email='cy.n01@outlook.com',
-        url='https://github.com/suisei-cn/pystargazer',
-        packages=find_packages(),
-        classifiers=[
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3',
-            'Operating System :: OS Independent',
-            'Development Status :: 3 - Alpha',
-            'Natural Language :: Chinese (Simplified)',
-            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-            'Intended Audience :: End Users/Desktop'
-        ],
-        install_requires=[
-            'uvloop',
-            'aiohttp',
-            'httpx',
-            'starlette',
-            'uvicorn[standard]',
-            'apscheduler',
-            'feedparser',
-            'python-dateutil',
-            'fastjsonschema'
-        ],
-        extras_require={
-            'mongo': ['motor'],
-            'files': ['tinydb==3.15.2'],
-            'telemetry': ['sentry-sdk']
-        },
-        python_requires='>=3.8'
+    name='pystargazer',
+    version='0.2.2',
+    description='A flexible vtuber tracker.',
+    author='LightQuantum',
+    author_email='cy.n01@outlook.com',
+    url='https://github.com/suisei-cn/pystargazer',
+    packages=find_namespace_packages(include=["pystargazer.*"]),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: Chinese (Simplified)',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Intended Audience :: End Users/Desktop'
+    ],
+    install_requires=[
+        'uvloop',
+        'aiohttp',
+        'httpx',
+        'starlette',
+        'uvicorn[standard]',
+        'apscheduler',
+        'feedparser',
+        'python-dateutil',
+        'fastjsonschema'
+    ],
+    extras_require={
+        'mongo': ['motor'],
+        'files': ['tinydb==3.15.2'],
+        'telemetry': ['sentry-sdk']
+    },
+    python_requires='>=3.8'
 )
